30bccfc35f05c91238a16a802079319a65c0f7d8
Anton Marhol <antonmarholbox@gmail.com>
2023-06-22 22:52:15 +0400

    fix a few grammatical errors

--- src/main/java/org/example/marhol/tasks/businessobjects/FirstTaskLogic.java
+++ src/main/java/org/example/marhol/tasks/businessobjects/FirstTaskLogic.java
@@ -6,7 +6,7 @@ public class FirstTaskLogic extends AbstractLogic {
     private static final String HELLO_MESSAGE = "Привет";
     private static final String WELCOME_MESSAGE = String.format("ДАВАЙТЕ УЗНАЕМ, ваше число больше %s ?", MAX_NUMBER);
     private static final String NUMBER_IS_LESS_MESSAGE = "Введенное число не больше " + MAX_NUMBER;
-    private static final String NUMBER_REQUEST_MESSAGE = "Введите ваше число или Q/q для выхода";
+    private static final String NUMBER_REQUEST_MESSAGE = "Введите ваше целое число или Q/q для выхода";
     private static final String NUMBER_REGEX = "\\d+";
 
     public void start() {
